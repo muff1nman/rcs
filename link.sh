62d6df1fbc649183072268b148992cdbf3a169d9
Andrew DeMaria <ademaria@mines.edu>
2013-08-22 13:57:48 -0600

    initial link stuff

---
+++ link.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# link.sh
+# Copyright (C) 2013 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+#
+# All Rights Reserved.
+#
+
+# for each directory, run dir/link.sh dir
+
+
